a07ffd5eeed9eab572b6b6ecd96f8899df4a1eae
Justin Winter <justinlevi@gmail.com>
2018-01-19 22:36:44 -0500

    adding in s3fs public/private prefix

--- src/SigningUtilities.php
+++ src/SigningUtilities.php
@@ -9,7 +9,7 @@ class SigningUtilities {
   /**
    * Returns the generated URL
    *
-   * @param String $filename
+   * @param array $filenames
    * @param \Aws\S3\S3ClientInterface $s3Client
    *   Client object.
    *
@@ -19,15 +19,22 @@ class SigningUtilities {
   static function generateUrls(Array $filenames, S3ClientInterface $s3Client){
 
     $presignedUrls = [];
-    $bucket = \Drupal::config('s3fs.settings')->get('bucket');
+    $config = \Drupal::config('s3fs.settings');
+    $bucket = $config->get('bucket');
+    $public_config = $config->get('public_folder');
+    $private_config = $config->get('private_folder');
+
+    $public_folder = !empty($public_config) ? $public_config : 's3fs-public';
+    $private_folder = !empty($private_config) ? $private_config : 's3fs-private';
 
     foreach ($filenames as $filename) {
 
       $cmd = $s3Client->getCommand('PutObject', [
-        'Bucket' => $bucket,
-        'Key' => $filename,
+        'Bucket'      => $bucket,
+        'Key'         => $public_folder . '/' . $filename,
         'ContentType' => 'image/jpeg',
         'Body'        => '',
+        'ACL'         => 'public-read',
       ]);
 
       $request = $s3Client->createPresignedRequest($cmd, '+5 minutes');
