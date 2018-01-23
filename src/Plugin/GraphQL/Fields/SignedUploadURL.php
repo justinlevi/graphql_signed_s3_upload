6cf645b0bdf2361ff109037ba0a7cbb9c27fc3c1
Justin Winter <justinlevi@gmail.com>
2018-01-23 15:15:01 -0500

    Broken :(

--- src/Plugin/GraphQL/Fields/SignedUploadURL.php
+++ src/Plugin/GraphQL/Fields/SignedUploadURL.php
@@ -9,7 +9,6 @@ use Drupal\graphql_signed_s3_upload\SigningUtilities;
 use Drupal\graphql\Plugin\GraphQL\PluggableSchemaBuilderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Youshido\GraphQL\Execution\ResolveInfo;
-use Aws\S3\S3Client;
 
 /**
  * List everything we've got in our page.
@@ -30,25 +29,17 @@ class SignedUploadURL extends FieldPluginBase implements ContainerFactoryPluginI
   use DependencySerializationTrait;
 
   /**
-   * The signing utility service class instance.
+   * The page instance.
    *
    * @var \Drupal\graphql_signed_s3_upload\SigningUtilities
    */
   protected $signingUtilities;
 
-
-  /**
-   * The AWS SDK for PHP S3Client object.
-   *
-   * @var \Aws\S3\S3Client
-   */
-  protected $s3 = NULL;
-
   /**
    * {@inheritdoc}
    */
   public function resolveValues($value, array $args, ResolveInfo $info) {
-    $result = $this->signingUtilities->generateUrls($args['input']['fileNames'], $this->s3);
+    $result = $this->signingUtilities->generateUrls($args['input']['fileNames']);
     foreach ($result as $signedUrl) {
       yield $signedUrl;
     }
@@ -58,27 +49,12 @@ class SignedUploadURL extends FieldPluginBase implements ContainerFactoryPluginI
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $pluginId, $pluginDefinition) {
-    $config = \Drupal::config('s3fs.settings')->get();
-    $client_config['credentials'] = [
-      'key' => $config['access_key'],
-      'secret' => $config['secret_key'],
-    ];
-
-    if (!empty($config['region'])) {
-      $client_config['region'] = $config['region'];
-      // Signature v4 is only required in the Beijing and Frankfurt regions.
-      // Also, setting it will throw an exception if a region hasn't been set.
-      $client_config['signature_version'] = 'v4';
-      $client_config['version'] = '2006-03-01';
-    }
-
-
     return new static(
         $configuration,
         $pluginId,
         $pluginDefinition,
-        $container->get('graphql_signed_s3_upload.signing_utilities'),
-        new S3Client($client_config));
+        $container->get('graphql_signed_s3_upload.signing_utilities')
+        );
   }
 
   /**
@@ -89,10 +65,8 @@ class SignedUploadURL extends FieldPluginBase implements ContainerFactoryPluginI
    * @param $pluginId
    * @param $pluginDefinition
    * @param \Drupal\graphql_signed_s3_upload\SigningUtilities $signingUtilities
-   * @param \Aws\S3\S3Client $s3
    */
-  public function __construct(array $configuration, $pluginId, $pluginDefinition, SigningUtilities $signingUtilities, S3Client $s3) {
-    $this->s3 = $s3;
+  public function __construct(array $configuration, $pluginId, $pluginDefinition, SigningUtilities $signingUtilities) {
     $this->signingUtilities = $signingUtilities;
     parent::__construct($configuration, $pluginId, $pluginDefinition);
   }
--- src/SigningUtilities.php
+++ src/SigningUtilities.php
@@ -2,45 +2,90 @@
 
 namespace Drupal\graphql_signed_s3_upload;
 
-use Aws\S3\S3ClientInterface;
+use \Aws\S3\S3ClientInterface;
+use \Aws\S3\S3Client;
 
 class SigningUtilities {
 
-  /**
-   * Returns the generated URL
-   *
-   * @param array $filenames
-   * @param \Aws\S3\S3ClientInterface $s3Client
-   *   Client object.
-   *
-   * @return array
-   *   The presigned URLs
-   */
-  static function generateUrls(Array $filenames, S3ClientInterface $s3Client){
+    /**
+     * The AWS SDK for PHP S3Client object.
+     *
+     * @var \Aws\S3\S3ClientInterface
+     */
+    protected $s3Client = NULL;
 
-    $presignedUrls = [];
-    $config = \Drupal::config('s3fs.settings');
-    $bucket = $config->get('bucket');
-    $public_config = $config->get('public_folder');
-    $private_config = $config->get('private_folder');
+    /**
+     *
+     */
+    protected $s3fsSettings = NULL;
 
-    $public_folder = !empty($public_config) ? $public_config : 's3fs-public';
-    $private_folder = !empty($private_config) ? $private_config : 's3fs-private';
-
-    foreach ($filenames as $filename) {
-
-      $cmd = $s3Client->getCommand('PutObject', [
-        'Bucket'      => $bucket,
-        'Key'         => $public_folder . '/' . $filename,
-        'ContentType' => 'image/jpeg',
-        'ACL'         => 'public-read',
-      ]);
-
-      $request = $s3Client->createPresignedRequest($cmd, '+5 minutes');
-      $presignedUrls[] =(string) $request->getUri();
-
+    /**
+     * Constructs a MediaImageUtilities object.
+     *
+     * @param \Aws\S3\S3ClientInterface $s3client
+     *   The s3 Client.
+     */
+    public function __construct() {
+        $this->s3fsSettings = $config = \Drupal::config('s3fs.settings')->get();
+        $this->s3Client = new S3Client($this->getClientConfig());
     }
 
-    return $presignedUrls;
-  }
+    /**
+     * Returns the generated URL
+     *
+     * @param array $filenames
+     *   The fullpath/filenames that we need presigned urls for.
+     *
+     * @return array
+     *   The presigned URLs
+     */
+    public function generateUrls(Array $filenames){
+        $presignedUrls = [];
+
+        $bucket = $this->s3fsSettings->get('bucket');
+        $public_config = $this->s3fsSettings->get('public_folder');
+        $private_config = $this->s3fsSettings->get('private_folder');
+
+        $public_folder = !empty($public_config) ? $public_config : 's3fs-public';
+
+        //TODO: handle private folders
+        $private_folder = !empty($private_config) ? $private_config : 's3fs-private';
+
+        foreach ($filenames as $filename) {
+
+            $cmd = $this->s3Client->getCommand('PutObject', [
+                'Bucket'      => $bucket,
+                'Key'         => $public_folder . '/' . $filename,
+                'ContentType' => 'image/jpeg',
+                'ACL'         => 'public-read',
+            ]);
+
+            $request = $this->s3Client->createPresignedRequest($cmd, '+5 minutes');
+            $presignedUrls[] =(string) $request->getUri();
+        }
+
+        return $presignedUrls;
+    }
+
+    /**
+     * Returns the S3fs S3 configuration
+     *
+     * @return array
+     */
+    protected function getClientConfig(){
+
+        $clientConfig['credentials'] = [
+            'key' => $this->s3fsSettings['access_key'],
+            'secret' => $this->s3fsSettings['secret_key'],
+        ];
+
+        if (!empty($this->s3fsSettings['region'])) {
+            $clientConfig['region'] = $this->s3fsSettings['region'];
+            // Signature v4 is only required in the Beijing and Frankfurt regions.
+            // Also, setting it will throw an exception if a region hasn't been set.
+            $clientConfig['signature_version'] = 'v4';
+            $clientConfig['version'] = '2006-03-01';
+        }
+        return $clientConfig;
+    }
 }
